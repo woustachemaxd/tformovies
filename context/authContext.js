d26e2e7859ff07239a04e834a16adc53ea387438
woustachemaxd <deepanshu1115@gmail.com>
2023-05-28 00:18:09 +0530

    deciding card

--- context/authContext.js
+++ context/authContext.js
@@ -1,11 +1,12 @@
 import React, { createContext, useState, useEffect } from "react";
-
+import data from "./testData.json";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const testValue = true;
+
   return (
-    <AuthContext.Provider value={{ testValue }}>
+    <AuthContext.Provider value={{ testValue, data }}>
       {children}
     </AuthContext.Provider>
   );
--- src/routes/inside-stack/Homepage.js
+++ src/routes/inside-stack/Homepage.js
@@ -1,13 +1,43 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { ScrollView, StyleSheet, Text, Image } from "react-native";
+import React, { useContext, useCallback } from "react";
+import { AuthContext } from "../../../context/authContext";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Homepage = () => {
+  const { data } = useContext(AuthContext);
+  const index = Math.floor(Math.random() * 8);
+  const [fontsLoaded] = useFonts({
+    "Everett-Regular": require("../../../assets/fonts/TWKEverett-Regular.otf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  console.log(data.result[index].posterURLs.original);
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "Everett-Regular", fontSize: 30 }}>
-        Homepage
-      </Text>
-    </View>
+    <>
+      <StatusBar style="auto" />
+      <SafeAreaView style={styles.container} onLayout={onLayoutRootView}>
+        <ScrollView style={styles.card}>
+          <Image
+            style={styles.img}
+            source={{
+              uri: data.result[index].posterURLs.original,
+            }}
+          />
+          <Text style={styles.header}>{data.result[index].title}</Text>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
@@ -15,8 +45,33 @@ export default Homepage;
 
 const styles = StyleSheet.create({
   container: {
+    borderWidth: 1,
+    borderColor: "red",
+    backgroundColor: "#f0f0f0",
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  card: {
+    width: "90%",
+    marginVertical: 30,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  img: {
+    width: "100%",
+    overflow: "hidden",
+    height: 300,
+    resizeMode: "cover",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+  },
+  header: {
+    fontFamily: "Everett-Regular",
+    fontSize: 23,
+    top: -20,
   },
 });
